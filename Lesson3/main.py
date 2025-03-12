fff2a8f76e9b98c1720a9c4d6ccde55a96123eec
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-03-12 12:06:03 +0500

    Update Main

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -79,25 +79,50 @@ app = FastAPI()
     
 # ------------------------------------------------------------------
 
-data = {
-    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
-    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
-    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
-}
+# data = {
+#     "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+#     "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+#     "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+# }
 
 
-def check_valid_id(id: str):
-    if not id.startswith(("isbn-", "imdb-")):
-        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
-    return id
+# def check_valid_id(id: str):
+#     if not id.startswith(("isbn-", "imdb-")):
+#         raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+#     return id
 
 
-@app.get("/items/")
-async def read_items(
-    id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
-):
-    if id:
-        item = data.get(id)
-    else:
-        id, item = random.choice(list(data.items()))
-    return {"id": id, "name": item}
+# @app.get("/items/")
+# async def read_items(
+#     id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+# ):
+#     if id:
+#         item = data.get(id)
+#     else:
+#         id, item = random.choice(list(data.items()))
+#     return {"id": id, "name": item}
+
+# ------------------------------------------------------------------
+
+# data = {
+#     "pdp-1234": "Nigmatjonov Muhammadjon",
+#     "pdp-12345": "Xasan Xoliqnazarov",
+#     "pdp-123456": "Ali Alimov",
+# }
+
+
+# def check_valid_id(id: str):
+#     if not id.startswith(("pdp-")):
+#         raise ValueError('Invalid ID format, it must start with "pdp-"')
+#     return id
+
+
+# @app.get("/items/")
+# async def read_items(
+#     id: Annotated[str | None, AfterValidator(check_valid_id)] = None,
+# ):
+#     if id:
+#         item = data.get(id)
+#     else:
+#         id, item = random.choice(list(data.items()))
+#     return {"id": id, "name": item}
