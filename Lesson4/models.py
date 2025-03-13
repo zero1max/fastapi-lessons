58fb500002582f9c02b6b49937c12f62d997957f
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-03-13 12:19:24 +0500

    Update Models

--- Lesson4/models.py
+++ Lesson4/models.py
@@ -1,8 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional, Any, List
 
 class User(BaseModel):
-    id: int | None = None  
+    id: Optional[int] = None  
     fullname: str
     username: str
     email: str
-    password: str
+    password: str = Field(..., exclude=True)  # Parolni himoya qilish
+
+class BaseResponseModel(BaseModel):
+    success: bool
+    data: Optional[Any] = None
+    errors: Optional[List[dict]] = None
